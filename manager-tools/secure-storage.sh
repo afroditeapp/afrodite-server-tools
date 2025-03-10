7f72e336c72924e67d3ba46401063bd8117f9696
Juuso Tuononen <jutuon@outlook.com>
2025-03-10 19:42:47 +0200

    Improve secure storage script
    - Add storage extending support.
    - Merge mount and unmount commands with open and close commands.
    - Exit with error status code when requested command is not supported.

--- manager-tools/secure-storage.sh
+++ manager-tools/secure-storage.sh
@@ -3,22 +3,18 @@
 DEFAULT_STORAGE_DIR="/afrodite-secure-storage"
 DEFAULT_STORAGE_PASSWORD="password"
 
-CMD_OPEN="open"
+CMD_EXTEND_SIZE_AND_OPEN="extend-size-and-open"
 CMD_CLOSE="close"
-CMD_MOUNT="mount"
-CMD_UNMOUNT="unmount"
 CMD_IS_DEFAULT_PASSWORD="is-default-password"
 CMD_CHANGE_DEFAULT_PASSWORD="change-default-password"
 
 function print_help_and_quit() {
-    echo "Usage: sudo bash -u secure-storage.sh COMMAND STORAGE_DIR"
+    echo "Usage: sudo bash -u secure-storage.sh COMMAND STORAGE_DIR [STORAGE_SIZE_IN_BYTES]"
     echo "Available commands:"
-    echo "  $CMD_OPEN - Password is read from standard input"
+    echo "  $CMD_EXTEND_SIZE_AND_OPEN - Password is read from standard input, required state: closed"
     echo "  $CMD_CLOSE"
-    echo "  $CMD_MOUNT"
-    echo "  $CMD_UNMOUNT"
-    echo "  $CMD_IS_DEFAULT_PASSWORD - Exits with value 0 when the password is the default password"
-    echo "  $CMD_CHANGE_DEFAULT_PASSWORD - Password is read from standard input"
+    echo "  $CMD_IS_DEFAULT_PASSWORD - Exits with value 0 when the password is the default password, required state: closed"
+    echo "  $CMD_CHANGE_DEFAULT_PASSWORD - Password is read from standard input, required state: closed"
     echo "Available storage directories: $DEFAULT_STORAGE_DIR"
     exit 1
 }
@@ -29,34 +25,38 @@ function setup_global_variables() {
         g_storage_dir="$1"
         g_encrypted_file="/afrodite-encrypted-filesystem/encrypted-filesystem.data"
         g_mapper_name="afrodite-encrypted-data-mapper"
+        g_mapper_file="/dev/mapper/$g_mapper_name"
     else
         echo "Unsupported storage directory '$1'"
         exit 1
     fi
 }
 
-# Input: STDIN = storage password
-function open_mapper_and_quit() {
+# Input: STDIN = storage password, $1 = STORAGE_SIZE_IN_BYTES
+function extend_size_and_open_storage_and_quit() {
     set -x
+
+    current_storage_size=$(stat --printf="%s" "$g_encrypted_file")
+    allocate_storage=$(test "$1" -gt "$current_storage_size")
+
+    if $allocate_storage; then
+        fallocate -l "$1" "$g_encrypted_file"
+    fi
+
     cryptsetup --key-file - luksOpen "$g_encrypted_file" "$g_mapper_name"
+
+    if $allocate_storage; then
+        resize2fs "$g_mapper_file"
+    fi
+
+    mount "$g_mapper_file" "$g_storage_dir"
     exit 0
 }
 
-function close_mapper_and_quit() {
-    set -x
-    cryptsetup luksClose "$g_mapper_name"
-    exit 0
-}
-
-function mount_and_quit() {
-    set -x
-    mount "/dev/mapper/$g_mapper_name" "$g_storage_dir"
-    exit 0
-}
-
-function unmount_and_quit() {
+function close_storage_and_quit() {
     set -x
     umount "$g_storage_dir"
+    cryptsetup luksClose "$g_mapper_name"
     exit 0
 }
 
@@ -107,12 +107,18 @@ fi
 
 setup_global_variables "$2"
 
+if [ $# -ge 3 ]; then
+    storage_size_in_bytes="$3"
+else
+    storage_size_in_bytes="0"
+fi
+
 case "$1" in
-    "$CMD_OPEN") open_mapper_and_quit ;;
-    "$CMD_CLOSE") close_mapper_and_quit ;;
-    "$CMD_MOUNT") mount_and_quit ;;
-    "$CMD_UNMOUNT") unmount_and_quit ;;
+    "$CMD_EXTEND_SIZE_AND_OPEN") extend_size_and_open_storage_and_quit "$storage_size_in_bytes" ;;
+    "$CMD_CLOSE") close_storage_and_quit ;;
     "$CMD_IS_DEFAULT_PASSWORD") is_default_password_and_quit ;;
     "$CMD_CHANGE_DEFAULT_PASSWORD") change_default_password_and_quit ;;
-    *) echo "Unsupported command '$1'" ;;
+    *) echo "Unsupported command '$1'"
+       exit 1
+    ;;
 esac
